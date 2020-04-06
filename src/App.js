957871bb3fc8b871fb0b459abef3d6cc52067de9
Rich Sturim <rich.sturim@coxautoinc.com>
2020-04-06 12:11:18 -0400

    Authenticator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import "./App.css";
+import { Auth, Hub } from "aws-amplify";
 
-class App extends React.Component {
-  state = {};
+import { Authenticator, AmplifyTheme } from "aws-amplify-react";
 
-  render() {
-    return <div>App</div>;
-  }
-}
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // getUserData();
+    Hub.listen("auth", this, onHubCapsule);
+  }, []);
+
+  const getUserData = async () => {
+    const user = await Auth.currentAuthenticatedUser();
+
+    if (user) {
+      setUser(user);
+    } else {
+      setUser(null);
+    }
+  };
+
+  const onHubCapsule = (capsule) => {
+    switch (capsule.payload.event) {
+      case "signIn":
+        console.log("signed in");
+        getUserData();
+        break;
+      case "signOut":
+        console.log("signed out");
+        setUser(null);
+        break;
+
+      default:
+        return;
+    }
+  };
+
+  return !user ? (
+    <Authenticator />
+  ) : (
+    <div>{user ? `Hello ${user.username}` : "no one found"}</div>
+  );
+};
 
 export default App;
+
+// const theme = {
+//   ...AmplifyTheme,
+//   navBar: {
+//     ...AmplifyTheme,
+//     backgroundColor: "#ccc",
+//   },
+//   button: {
+//     ...AmplifyTheme.button,
+//     backgroundColor: "var(--amazonOrange)",
+//     color: "white",
+//   },
+//   sectionHeader: {
+//     ...AmplifyTheme,
+//     backgroundColor: "#eee",
+//     padding: "10px",
+//   },
+// };
+
+// export default withAuthenticator(App, true, [], null, theme);
